bb314eb64cf6b62dc377f11035a1d589691838aa
Vishwa <vishwa.m2204@gmail.com>
2023-12-13 00:02:09 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "../src/components/navbar/Navbar";
+import Home from "../src/pages/home";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../navbar/Navbar.css'
+
+function Navbar() {
+  return (
+   <>
+   <h1>Hi</h1>
+   </>
+  )
+}
+
+export default Navbar
